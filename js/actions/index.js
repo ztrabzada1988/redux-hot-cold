98b5c76124937bcb6f305896d275aade7e900110
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-03-01 19:48:07 -0800

    updating reducers and actions

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,11 +1,19 @@
+
 //action to guess a number
+export const GUESS_NUMBER = 'GUESS_NUMBER';
+export default function guessNumber(guess) {
+  type: GUESS_NUMBER,
+  guess: guess
+};
 
-//action to keep track of previous guesses
+//action to start a new game
+export const NEW_GAME = 'NEW_GAME';
+export default function newGame() {
+  type: NEW_GAME
+};
 
-//action to keep track of guesses
-
-//action to alert if wrong input is entered
-
-//action if the correct number is guessed
-
-//action for computer to generate a new random number
+//action when game is over
+export const GAME_OVER = 'GAME_OVER';
+export default function gameOver() {
+  type: GAME_OVER
+};
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,8 +1,23 @@
 import Actions from '../actions/index';
 
-const initialRepositoryState = [];
+const initialState = {
+  generateRandomNumber: (Math.floor(Math.random() * 100) + 1),
+  numberOfGuesses: 0,
+  guessedNumbers: [],
+  gameOver: false
+}
 
 export default function repositoryReducer(state, action) {
-    state = state || initialRepositoryState;
+    state = state || initialState;
+
+    if (action.type === Actions.NEW_GAME) {
+      return initialState;
+    } else if (action.type === Actions.GAME_OVER) {
+      initialState.gameOver = true;
+    } else if (action.type === Actions.GUESS_NUMBER) {
+      numberOfGuesses++
+      return guessedNumbers.push(guess);
+    }
+
     return state;
 };
