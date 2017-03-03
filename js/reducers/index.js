e47aa2d7aaaf4aef5f49c222cce8becf0c688f23
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-03-03 15:36:26 -0800

    updating reducers and actions

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -4,22 +4,22 @@ const initialState = {
   generateRandomNumber: (Math.floor(Math.random() * 100) + 1),
   numberOfGuesses: 0,
   guessedNumbers: [],
-  gameOver: false
+  gameOver: false,
+  difference: 0
 }
 
 export default function repositoryReducer(state, action) {
     state = state || initialState;
 
     if (action.type === Actions.NEW_GAME) {
-      return initialState;
-      console.log(initialState);
+        state = initialState;
     } else if (action.type === Actions.GAME_OVER) {
       initialState.gameOver = true;
     } else if (action.type === Actions.GUESS_NUMBER) {
       numberOfGuesses++
-      console.log(numberOfGuesses);
-      return guessedNumbers.push(guess);
-      console.log(guessedNumbers);
+
+      guessedNumbers.push(guess);
+      return
     }
     return state;
 };
