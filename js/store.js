cc1cf3073a3c5cafbc6f9e1ca056a7ed6f8750f3
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-03-14 23:36:57 -0700

    creating and updating components

--- js/store.js
+++ js/store.js
@@ -2,10 +2,7 @@
 //import {createStore, applyMiddleware, compose} from 'redux';
 //import logger from 'redux-logger';
 //import reducer from './reducers';
-//
-//
 //const CreateStore = Redux.createStore;
-//
 //export const Store = CreateStore(Reducers.HotColdReducer);
 
 import {createStore, applyMiddleware, compose} from 'redux';
